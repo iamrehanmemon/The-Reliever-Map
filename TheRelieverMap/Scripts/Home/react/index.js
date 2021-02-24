9dfe70a1ec77fa15f6d38cb177a7363d846f1020
Rehan Memon <rehan4747@gmail.com>
2021-02-24 17:32:13 +0530

    Removed Nav-Conflicting routing

--- TheRelieverMap/Scripts/Home/react/index.js
+++ TheRelieverMap/Scripts/Home/react/index.js
@@ -3,19 +3,16 @@ import { render } from 'react-dom';
 import { BrowserRouter, Route } from "react-router-dom";
 
 import HomePage from "../react/pages/homepage/homepage.component"
-import AddFormToilet from './components/addformtoilet/addformtoilet.component';
-import Navbar from './components/navbar/navbar.component';
+
 
 
 
 const App = () => (
     <React.Fragment>
         <BrowserRouter>
-            <Navbar/>
             <Route path="/" exact>
                 <HomePage />
             </Route>
-            <Route path="/AddToilet/Add" component={ AddFormToilet}/>
         </BrowserRouter>
     </React.Fragment>
 );
